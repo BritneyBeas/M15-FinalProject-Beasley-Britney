8be97639ee14a77f9bbedd03527943398a51ee26
BritneyBeas <britneyo17@yahoo.com>
2022-05-05 19:13:21 -0400

    Initial commit

---
+++ src/main/java/com/company/WeatherDetail.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class WeatherDetail {
+
+    public int id;
+    public String main;
+    public String description;
+    public String icon;
+}
---
+++ src/main/java/com/company/WeatherResponse.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class WeatherResponse {
+
+    public Coordinate coord;
+    public WeatherDetail[] weather;
+    public String base;
+
+}
